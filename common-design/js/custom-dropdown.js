afb018ecf4f426c3b51bf9834049f0c428336b68
left23 <lcdemc@gmail.com>
2018-07-26 14:30:08 +0200

    adjust selector for click event to match Drupal class for main menu item with nested menu

--- common-design/js/custom-dropdown.js
+++ common-design/js/custom-dropdown.js
@@ -1,7 +1,7 @@
 // @see https://www.drupal.org/docs/7/theming/working-with-javascript-and-jquery
 (function($) {
   $(document).ready(function(){
-    $('.cd-dropdown .cd-dropdown-toggle').on('click', function(e){
+    $('.cd-dropdown .expanded').on('click', function(e){
       $(this).parent().toggleClass('open');
       e.stopPropagation();
       e.preventDefault();
