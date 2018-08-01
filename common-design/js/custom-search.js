0e14286d821ceeac4eb06a775a2c50773f322227
left23 <lcdemc@gmail.com>
2018-08-01 10:23:23 +0200

    add parent selector so js class is not added to search--inline component

--- common-design/js/custom-search.js
+++ common-design/js/custom-search.js
@@ -1,11 +1,11 @@
 // @see https://www.drupal.org/docs/7/theming/working-with-javascript-and-jquery
 (function($) {
   $(document).ready(function(){
-    $('.cd-search__input').on('focus', function(e){
+    $('.cd-search .cd-search__input').on('focus', function(e){
       $(this).parent().find('.cd-search__submit').toggleClass('js-has-focus');
     });
 
-    $('.cd-search__input').on('blur', function(e){
+    $('.cd-search .cd-search__input').on('blur', function(e){
       $(this).parent().find('.cd-search__submit').toggleClass('js-has-focus');
     });
   });
