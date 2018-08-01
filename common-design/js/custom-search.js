023296612a4dc54c7d6933385de394acdc7e54b6
left23 <lcdemc@gmail.com>
2018-08-01 12:29:00 +0200

    search--input equivalent of adding class to form inner div and button when input has focus

--- common-design/js/custom-search.js
+++ common-design/js/custom-search.js
@@ -1,6 +1,9 @@
 // @see https://www.drupal.org/docs/7/theming/working-with-javascript-and-jquery
 (function($) {
   $(document).ready(function(){
+
+    // search
+
     $('.cd-search .cd-search__input').on('focus', function(e){
       $(this).parent().find('.cd-search__submit').toggleClass('js-has-focus');
     });
@@ -8,5 +11,24 @@
     $('.cd-search .cd-search__input').on('blur', function(e){
       $(this).parent().find('.cd-search__submit').toggleClass('js-has-focus');
     });
+
+    // search--inline
+
+    $('.cd-search--inline .cd-search--inline__input').on('focus', function(e){
+      $(this).parent().find('.cd-search--inline__submit').toggleClass('js-has-focus');
+    });
+
+    $('.cd-search--inline .cd-search--inline__input').on('blur', function(e){
+      $(this).parent().find('.cd-search--inline__submit').toggleClass('js-has-focus');
+    });
+
+    $('.cd-search--inline .cd-search--inline__input').on('focus', function(e){
+      $(this).parent().toggleClass('js-has-focus');
+    });
+
+    $('.cd-search--inline .cd-search--inline__input').on('blur', function(e){
+      $(this).parent().toggleClass('js-has-focus');
+    });
+
   });
 })(jQuery);
