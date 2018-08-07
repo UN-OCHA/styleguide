59e4b99d499aec6ba22ed4ab91d1acbc99dd0a88
left23 <lcdemc@gmail.com>
2018-08-07 08:40:31 +0200

    separate js for search variants

--- common-design/js/custom-search.js
+++ common-design/js/custom-search.js
@@ -3,31 +3,12 @@
   $(document).ready(function(){
 
     // search
-
     $('.cd-search .cd-search__input').on('focus', function(e){
       $(this).parent().find('.cd-search__submit').toggleClass('js-has-focus');
     });
 
     $('.cd-search .cd-search__input').on('blur', function(e){
       $(this).parent().find('.cd-search__submit').toggleClass('js-has-focus');
-    });
-
-    // search--inline
-
-    $('.cd-search--inline .cd-search--inline__input').on('focus', function(e){
-      $(this).parent().find('.cd-search--inline__submit').toggleClass('js-has-focus');
-    });
-
-    $('.cd-search--inline .cd-search--inline__input').on('blur', function(e){
-      $(this).parent().find('.cd-search--inline__submit').toggleClass('js-has-focus');
-    });
-
-    $('.cd-search--inline .cd-search--inline__input').on('focus', function(e){
-      $(this).parent().toggleClass('js-has-focus');
-    });
-
-    $('.cd-search--inline .cd-search--inline__input').on('blur', function(e){
-      $(this).parent().toggleClass('js-has-focus');
     });
 
   });
