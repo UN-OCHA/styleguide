c7251d57642abda6cf250f7105b10287879347b8
left23 <lcdemc@gmail.com>
2018-08-10 12:52:16 +0200

    hook into bootstrap dropdown events so search input gains focus when search button to expose the form is clicked, copied same to search inline relevant only on < tablet, tidy sass CS and better colour control for toggle button

--- common-design/js/custom-search.js
+++ common-design/js/custom-search.js
@@ -2,13 +2,22 @@
 (function($) {
   $(document).ready(function(){
 
-    // search
-    $('.cd-search .cd-search__input').on('focus', function(e){
-      $(this).parent().find('.cd-search__submit').toggleClass('js-has-focus');
+    // Apply focus to input when dropdown is shown.
+    $('.cd-search').on('shown.bs.dropdown', function () {
+      $(this).find('#cd-search').focus();
     });
 
-    $('.cd-search .cd-search__input').on('blur', function(e){
-      $(this).parent().find('.cd-search__submit').toggleClass('js-has-focus');
+    $('.cd-search').on('hidden.bs.dropdown', function () {
+      $(this).find('#cd-search').blur();
+    });
+
+    // Add class on submit button when input has focus.
+    $('.cd-search .cd-search__input').on('focus', function(e) {
+      $(this).parent().find('.cd-search__submit').addClass('js-has-focus');
+    });
+
+    $('.cd-search .cd-search__input').on('blur', function(e) {
+      $(this).parent().find('.cd-search__submit').removeClass('js-has-focus');
     });
 
   });
